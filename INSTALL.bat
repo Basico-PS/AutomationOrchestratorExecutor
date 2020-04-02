b12eb2ece61b493a9b3daf79c953757b978af04f
Mathias Balsløw <49169557+mbalslow@users.noreply.github.com>
2020-04-02 10:54:00 +0200

    @echo off and implementation of better logic

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,4 +1,9 @@
+@echo off
+
 call cd %~dp0
 call python -m venv venv
 call venv\scripts\activate
 call pip install -r requirements.txt --no-cache-dir
+
+echo The installation is now complete!
+TIMEOUT 15
