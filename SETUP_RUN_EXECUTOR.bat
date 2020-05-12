f52a1764328242d060d72d588828a133412ecac6
Mathias Balsløw <mbalslow@basico.dk>
2020-05-12 13:00:37 +0200

    Add option to run the application with pythonw (hidden)

--- SETUP_RUN_EXECUTOR.bat
+++ SETUP_RUN_EXECUTOR.bat
@@ -5,6 +5,9 @@ CALL CD %~dp0
 echo It is recommended NOT to run with highest privileges unless it is necessary for the command to be executed.
 CALL SET /P privileges=Would you like to run the executor with highest privileges (Y/N)?
 
+echo It is recommended NOT to run the application hidden. Only experienced users should run it hidden!
+CALL SET /P hidden=Would you like to run the application hidden (Y/N)?
+
 IF %privileges% == Y (
     CALL SET level=HIGHEST
 ) else (
@@ -15,5 +18,15 @@ IF %privileges% == Y (
     )
 )
 
-CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorExecutorRunExecutor" /TR "'%CD%\venv\scripts\python.exe' '%CD%\automation_orchestrator_executor\automation_orchestrator_executor.py'" /ST 00:00 /RI 1 /DU 23:59 /RL %level% /F
+IF %hidden% == Y (
+    CALL SET python=pythonw.exe
+) else (
+    IF %hidden% == y (
+        CALL SET python=pythonw.exe
+    ) else (
+        CALL SET python=python.exe
+    )
+)
+
+CALL SCHTASKS /CREATE /SC DAILY /TN "AutomationOrchestratorExecutorRunExecutor" /TR "'%CD%\venv\scripts\%python%' '%CD%\automation_orchestrator_executor\automation_orchestrator_executor.py'" /ST 00:00 /RI 1 /DU 23:59 /RL %level% /F
 TIMEOUT 15
