65d12c67c05a23db6af71a6f59f85593a6135633
Mathias Balsløw <49169557+mbalslow@users.noreply.github.com>
2021-11-30 00:07:45 +0100

    Update INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -3,6 +3,7 @@
 call cd %~dp0
 call python -m venv venv
 call venv\scripts\activate
+CALL python -m pip install --upgrade pip
 call pip install -r requirements.txt --no-cache-dir
 
 echo The installation is now complete!
